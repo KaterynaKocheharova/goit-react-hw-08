89925798b064d0c0a2cec09586385179afcef745
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-06-18 12:30:17 +0300

    add toggle editing mode

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { CiUser } from "react-icons/ci";
-import { CiPhone } from "react-icons/ci";
+import { CiUser, CiPhone } from "react-icons/ci";
 import CustomModal from "../Modal/Modal";
 import css from "./Contact.module.css";
 import { useDispatch } from "react-redux";
@@ -9,6 +8,24 @@ import { activateSuccessToast } from "../../js/toast";
 
 export default function Contact({ contactData: { name, number, id } }) {
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [isEditingNumber, setIsEditingNumber] = useState(false);
+  const [inputData, setInputData] = useState({ name, number });
+
+  const toggleEditingName = () => {
+    setIsEditingName((prev) => !prev);
+  };
+
+  const toggleEditingNumber = () => {
+    setIsEditingNumber((prev) => !prev);
+  };
+
+  const editData = (e) => {
+    setInputData((data) => ({
+      ...data,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   const dispatch = useDispatch();
 
@@ -31,11 +48,37 @@ export default function Contact({ contactData: { name, number, id } }) {
         <div className={css["contact-info-wrapper"]}>
           <div className={css["item-icon-box"]}>
             <CiUser className={css["contact-person-icon"]} />
-            <p className={css["name-text"]}>{name}</p>
+            {isEditingName ? (
+              <input
+                type="text"
+                value={inputData.name}
+                onChange={editData}
+                name="name"
+                onBlur={toggleEditingName}
+                autoFocus
+              />
+            ) : (
+              <p className={css["name-text"]} onClick={toggleEditingName}>
+                {name}
+              </p>
+            )}
           </div>
           <div className={css["item-icon-box"]}>
             <CiPhone className={css["contact-phone-icon"]} />
-            <p className={css["number-text"]}>{number}</p>
+            {isEditingNumber ? (
+              <input
+                type="number"
+                name="number"
+                value={inputData.number}
+                onChange={editData}
+                onBlur={toggleEditingNumber}
+                autoFocus
+              />
+            ) : (
+              <p className={css["number-text"]} onClick={toggleEditingNumber}>
+                {number}
+              </p>
+            )}
           </div>
         </div>
         <button className={css["delete-button"]} onClick={openModal}>
