46a74c89c74e12b54c53e52c6b1e273d3021dc9e
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-06-23 14:16:51 +0300

    qqwer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react";
 import { CiUser, CiPhone } from "react-icons/ci";
 import CustomModal from "../Modal/Modal";
@@ -11,7 +10,7 @@ export default function Contact({ contactData: { name, number, id } }) {
   const [modalIsOpen, setIsOpen] = useState(false);
   const [isEditing, setIsEditing] = useState(null); // 'name' or 'number' or null
   const [contactData, setContactData] = useState({ name, number, id });
-  const [modalType, setModalType] = useState("")
+  const [modalType, setModalType] = useState("");
   const dispatch = useDispatch();
 
   const toggleEditing = (field) => {
@@ -19,12 +18,12 @@ export default function Contact({ contactData: { name, number, id } }) {
   };
 
   useEffect(() => {
-    if(isEditing) {
-      setModalType("editingContactModal")
+    if (isEditing) {
+      setModalType("editingContactModal");
     } else {
-      setModalType("deleteContactModal")
-    } 
-  },[isEditing])
+      setModalType("deleteContactModal");
+    }
+  }, [isEditing]);
 
   const editData = (e) => {
     setContactData((data) => ({
@@ -39,70 +38,72 @@ export default function Contact({ contactData: { name, number, id } }) {
 
   const doUpdateContact = () => {
     dispatch(updateContact(contactData))
-    .unwrap()
-    .then(() => {
-      activateSuccessToast("Contact successfully updated");
-      setIsEditing(false)})
-    .catch((error) => activateErrorToast(error));
-  }
+      .unwrap()
+      .then(() => {
+        activateSuccessToast("Contact successfully updated");
+        setIsEditing(false);
+      })
+      .catch((error) => activateErrorToast(error));
+  };
 
   function doDeleteContact() {
     dispatch(deleteContact(id))
-    .unwrap()
-    .then(() => {
-      activateSuccessToast("Contact successfully deleted");
-    });
+      .unwrap()
+      .then(() => {
+        activateSuccessToast("Contact successfully deleted");
+      });
   }
 
   function closeModal() {
     setIsOpen(false);
-   
   }
 
   return (
     <>
       <li className={css["contact-item"]}>
         <div className={css["contact-info-wrapper"]}>
-          <div className={css["item-icon-box"]}>
-            <CiUser className={css["contact-person-icon"]} />
-            {isEditing === "name" ? (
-              <input
-                type="text"
-                value={contactData.name}
-                onChange={editData}
-                name="name"
-                // onBlur={handleOnBlur}
-                autoFocus
-              />
-            ) : (
+          {isEditing === "name" ? (
+            <input
+              className={css["contact-input"]}
+              type="text"
+              value={contactData.name}
+              onChange={editData}
+              name="name"
+              onBlur={() => setIsEditing(false)}
+              autoFocus
+            />
+          ) : (
+            <div className={css["item-icon-box"]}>
+              <CiUser className={css["contact-person-icon"]} />
               <p
                 className={css["name-text"]}
                 onClick={() => toggleEditing("name")}
               >
                 {contactData.name}
               </p>
-            )}
-          </div>
-          <div className={css["item-icon-box"]}>
-            <CiPhone className={css["contact-phone-icon"]} />
-            {isEditing === "number" ? (
-              <input
-                type="number"
-                name="number"
-                value={contactData.number}
-                onChange={editData}
-                // onBlur={handleOnBlur}
-                autoFocus
-              />
-            ) : (
+            </div>
+          )}
+          {isEditing === "number" ? (
+            <input
+              className={css["contact-input"]}
+              type="number"
+              name="number"
+              value={contactData.number}
+              onChange={editData}
+              onBlur={() => setIsEditing(false)}
+              autoFocus
+            />
+          ) : (
+            <div className={css["item-icon-box"]}>
+              <CiPhone className={css["contact-phone-icon"]} />
               <p
                 className={css["number-text"]}
                 onClick={() => toggleEditing("number")}
               >
                 {contactData.number}
               </p>
-            )}
-          </div>
+            </div>
+          )}
         </div>
         <button className={css["delete-button"]} onClick={openModal}>
           {isEditing ? "Update" : "Delete"}
@@ -117,5 +118,3 @@ export default function Contact({ contactData: { name, number, id } }) {
     </>
   );
 }
-
-
