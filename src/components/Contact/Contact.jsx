413e033c0d1e0d69b107db4c7041f3b9dae97a4f
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-06-28 13:39:46 +0300

    add discard changes on blur

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,3 +1,146 @@
+
+// import { useState, useEffect, useRef } from "react";
+// import { CiUser, CiPhone } from "react-icons/ci";
+// import CustomModal from "../Modal/Modal";
+// import css from "./Contact.module.css";
+// import { useDispatch } from "react-redux";
+// import { deleteContact, updateContact } from "../../redux/contacts/operations";
+// import { activateErrorToast, activateSuccessToast } from "../../js/toast";
+
+// export default function Contact({ contactData: initialContactData }) {
+//   const [modalIsOpen, setIsOpen] = useState(false);
+//   const [isEditing, setIsEditing] = useState(null);
+//   const [contactData, setContactData] = useState(initialContactData);
+//   const [modalType, setModalType] = useState("");
+//   const dispatch = useDispatch();
+//   const nameInputRef = useRef(null);
+//   const numberInputRef = useRef();
+//   const buttonRef = useRef();
+
+//   const toggleEditing = (field) => {
+//     setIsEditing((prev) => (prev === field ? null : field));
+//   };
+
+//   useEffect(() => {
+//     if (isEditing) {
+//       setModalType("editingContactModal");
+//     } else {
+//       setModalType("deleteContactModal");
+//     }
+//   }, [isEditing]);
+
+//   const editData = (e) => {
+//     setContactData((data) => ({
+//       ...data,
+//       [e.target.name]: e.target.value,
+//     }));
+//   };
+
+//   function openModal() {
+//     setIsOpen(true);
+//     document.body.style.overflow = "hidden";
+//   }
+
+//   const doUpdateContact = () => {
+//     dispatch(updateContact(contactData))
+//       .unwrap()
+//       .then(() => {
+//         activateSuccessToast("Contact successfully updated");
+//         setIsEditing(false);
+//       })
+//       .catch((error) => activateErrorToast(error));
+//   };
+
+//   function doDeleteContact() {
+//     dispatch(deleteContact(initialContactData.id))
+//       .unwrap()
+//       .then(() => {
+//         activateSuccessToast("Contact successfully deleted");
+//       });
+//   }
+
+//   function closeModal() {
+//     setIsOpen(false);
+//     document.body.style.overflow = "auto";
+//   }
+
+//   const handleBlur = (e) => {
+//     if (e.relatedTarget === buttonRef.current) {
+//       return;
+//     }
+//     setContactData(initialContactData);
+//     setIsEditing(null);
+//   };
+
+//   return (
+//     <>
+//       <li className={css["contact-item"]}>
+//         <div className={css["contact-info-wrapper"]}>
+//           {isEditing === "name" ? (
+//             <input
+//               ref={nameInputRef}
+//               className={css["contact-input"]}
+//               type="text"
+//               value={contactData.name}
+//               onChange={editData}
+//               name="name"
+//               onBlur={handleBlur}
+//               autoFocus
+//             />
+//           ) : (
+//             <div className={css["item-icon-box"]}>
+//               <CiUser className={css["contact-person-icon"]} />
+//               <p
+//                 className={css["name-text"]}
+//                 onClick={() => toggleEditing("name")}
+//               >
+//                 {contactData.name}
+//               </p>
+//             </div>
+//           )}
+//           {isEditing === "number" ? (
+//             <input
+//               ref={numberInputRef}
+//               className={css["contact-input"]}
+//               type="number"
+//               name="number"
+//               value={contactData.number}
+//               onChange={editData}
+//               autoFocus
+//               onBlur={handleBlur}
+//             />
+//           ) : (
+//             <div className={css["item-icon-box"]}>
+//               <CiPhone className={css["contact-phone-icon"]} />
+//               <p
+//                 className={css["number-text"]}
+//                 onClick={() => toggleEditing("number")}
+//               >
+//                 {contactData.number}
+//               </p>
+//             </div>
+//           )}
+//         </div>
+//         <button
+//           ref={buttonRef}
+//           className={css["delete-button"]}
+//           onClick={openModal}
+//         >
+//           {isEditing ? "Update" : "Delete"}
+//         </button>
+//       </li>
+//       <CustomModal
+//         closeModal={closeModal}
+//         modalIsOpen={modalIsOpen}
+//         type={modalType}
+//         doSomething={isEditing ? doUpdateContact : doDeleteContact}
+//       />
+//     </>
+//   );
+// }
+
+
+
 import { useState, useEffect, useRef } from "react";
 import { CiUser, CiPhone } from "react-icons/ci";
 import CustomModal from "../Modal/Modal";
@@ -6,11 +149,12 @@ import { useDispatch } from "react-redux";
 import { deleteContact, updateContact } from "../../redux/contacts/operations";
 import { activateErrorToast, activateSuccessToast } from "../../js/toast";
 
-export default function Contact({ contactData: { name, number, id } }) {
+export default function Contact({ contactData: initialContactData }) {
   const [modalIsOpen, setIsOpen] = useState(false);
   const [isEditing, setIsEditing] = useState(null);
-  const [contactData, setContactData] = useState({ name, number, id });
+  const [contactData, setContactData] = useState(initialContactData);
   const [modalType, setModalType] = useState("");
+  // const [discardField, setDiscardField] = useState(null); 
   const dispatch = useDispatch();
   const nameInputRef = useRef(null);
   const numberInputRef = useRef();
@@ -51,7 +195,7 @@ export default function Contact({ contactData: { name, number, id } }) {
   };
 
   function doDeleteContact() {
-    dispatch(deleteContact(id))
+    dispatch(deleteContact(initialContactData.id))
       .unwrap()
       .then(() => {
         activateSuccessToast("Contact successfully deleted");
@@ -67,7 +211,15 @@ export default function Contact({ contactData: { name, number, id } }) {
     if (e.relatedTarget === buttonRef.current) {
       return;
     }
+    // setDiscardField(isEditing);
+    setModalType("discardingChangesModal");
+    openModal();
+  };
+
+  const discardChanges = () => {
+    setContactData(initialContactData);
     setIsEditing(null);
+    // closeModal();
   };
 
   return (
@@ -131,7 +283,9 @@ export default function Contact({ contactData: { name, number, id } }) {
         closeModal={closeModal}
         modalIsOpen={modalIsOpen}
         type={modalType}
-        doSomething={isEditing ? doUpdateContact : doDeleteContact}
+        doSomething={
+          modalType === "discardingChangesModal" ? discardChanges : isEditing ? doUpdateContact : doDeleteContact
+        }
       />
     </>
   );
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,95 @@
+// import Modal from "react-modal";
+// import css from "./Modal.module.css";
+// import Button from "../common/Button/Button";
+
+// Modal.setAppElement("#App");
+
+// const buildModalText = (type) => {
+//   switch (type) {
+//     case "deleteContactModal":
+//       return " Are you sure you want to delete the contact? Click delete if so.";
+//     case "editingContactModal":
+//       return "Are you sure you want to edit the contact? You won't be able to get the previous version";
+//     case "discardingChangesModal":
+//       return "Discard your changes?";
+//     default:
+//       return "";
+//   }
+// };
+
+// const buildModalButtonText = (type) => {
+//   switch (type) {
+//     case "deleteContactModal":
+//       return "delete";
+//     case "editingContactModal":
+//       return "update";
+//     case "discardingChangesModal":
+//       return "discard";
+//     default:
+//       return "";
+//   }
+// }
+
+// const CustomModal = ({ closeModal, modalIsOpen, type, doSomething }) => {
+//   return (
+//     <Modal
+//       className={css.modal}
+//       isOpen={modalIsOpen}
+//       onRequestClose={closeModal}
+//     >
+//       <>
+//         <div>
+//           {buildModalText(type)}
+//         </div>
+//         <Button
+//           onClick={() => {
+//             closeModal();
+//             doSomething();
+//           }}
+//           type="modal-window"
+//         >
+//           {buildModalButtonText(type)}
+//         </Button>
+//       </>
+//     </Modal>
+//   );
+// };
+
+// export default CustomModal;
+
+
+
 import Modal from "react-modal";
 import css from "./Modal.module.css";
 import Button from "../common/Button/Button";
 
 Modal.setAppElement("#App");
+
+const buildModalText = (type) => {
+  switch (type) {
+    case "deleteContactModal":
+      return "Are you sure you want to delete the contact? Click delete if so.";
+    case "editingContactModal":
+      return "Are you sure you want to edit the contact? You won't be able to get the previous version";
+    case "discardingChangesModal":
+      return "Discard your changes?";
+    default:
+      return "";
+  }
+};
+
+const buildModalButtonText = (type) => {
+  switch (type) {
+    case "deleteContactModal":
+      return "delete";
+    case "editingContactModal":
+      return "update";
+    case "discardingChangesModal":
+      return "discard";
+    default:
+      return "";
+  }
+};
 
 const CustomModal = ({ closeModal, modalIsOpen, type, doSomething }) => {
   return (
@@ -12,11 +99,7 @@ const CustomModal = ({ closeModal, modalIsOpen, type, doSomething }) => {
       onRequestClose={closeModal}
     >
       <>
-        <div>
-          {type === "deleteContactModal"
-            ? " Are you sure you want to delete the contact? Click delete if so."
-            : "Are you sure you want to edit the contact? You won't be able to get the previous version"}
-        </div>
+        <div>{buildModalText(type)}</div>
         <Button
           onClick={() => {
             closeModal();
@@ -24,9 +107,7 @@ const CustomModal = ({ closeModal, modalIsOpen, type, doSomething }) => {
           }}
           type="modal-window"
         >
-          {type === "deleteContactModal"
-            ? "Delete"
-            : "Update"}
+          {buildModalButtonText(type)}
         </Button>
       </>
     </Modal>
