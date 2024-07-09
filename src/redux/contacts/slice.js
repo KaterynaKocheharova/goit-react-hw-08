baf7f96a6ebb2b6d77acc068df056d9d2a759ea4
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-09 13:53:40 +0300

    finish main work

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -11,13 +11,13 @@ export const handlePending = (state) => {
 };
 
 export const handleError = (state, action) => {
-  // state.loading = false;
+  state.loading = false;
   state.error = action.payload;
 };
 
 const contactsInitialState = {
   items: [],
-  loading: true,
+  loading: false,
   error: null,
 };
 
@@ -29,19 +29,19 @@ const contactsSlice = createSlice({
       .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, action) => {
         state.error = null;
-        // state.loading = false;
+        state.loading = false;
         state.items = action.payload;
       })
       .addCase(fetchContacts.rejected, handleError)
       .addCase(addContact.pending, handlePending)
       .addCase(addContact.fulfilled, (state, action) => {    
         state.items.push(action.payload);
-        // state.loading = false;
+        state.loading = false;
       })
       .addCase(addContact.rejected, handleError)
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
-        // state.loading = false;
+        state.loading = false;
         state.error = null;
         const index = state.items.findIndex(
           (item) => item.id === action.payload.id
@@ -51,7 +51,7 @@ const contactsSlice = createSlice({
       .addCase(deleteContact.rejected, handleError)
       .addCase(updateContact.pending, handlePending)
       .addCase(updateContact.fulfilled, (state, action) => {
-        // state.loading = false;
+        state.loading = false;
         state.error = null;
         state.items = state.items.map((item) => {
           if (item.id === action.payload.id) {
